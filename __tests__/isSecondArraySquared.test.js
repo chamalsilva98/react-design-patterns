8b90430f258ebcc7e308fce4ac9ef61f3eb5c255
Chamal Silva <chamalsilva98@gmail.com>
2022-04-19 22:50:45 +0530

    added frequency pattern, fixes #7

--- __tests__/isSecondArraySquared.test.js
+++ __tests__/isSecondArraySquared.test.js
@@ -1,16 +1,16 @@
 import isSecondArraySquared from "../src/isSecondArraySquared";
 
-describe("Check second array is doubled", () => {
+describe("Check second array is squared", () => {
     test("none array should throw an error", () => {
         expect(() => isSecondArraySquared({}, { key: "value" })).toThrow();
     });
-    test("[1, 2, 3] and [4, 1, 9] shuld be true", () => {
+    test("[1, 2, 3] and [4, 1, 9] should be true", () => {
         expect(isSecondArraySquared([1, 2, 3], [4, 1, 9])).toBeTruthy();
     });
-    test("[1, 2, 3] and [1, 9] shuld be false", () => {
+    test("[1, 2, 3] and [1, 9] should be false", () => {
         expect(isSecondArraySquared([1, 2, 3], [1, 9])).toBeFalsy();
     });
-    test("[1, 2, 1] and [4, 4, 1] shuld be false", () => {
+    test("[1, 2, 1] and [4, 4, 1] should be false", () => {
         expect(isSecondArraySquared([1, 2, 1], [4, 4, 1])).toBeFalsy();
     });
 });
--- src/isSecondArraySquared.js
+++ src/isSecondArraySquared.js
@@ -4,19 +4,42 @@
  * @param {number[]} secondArray
  */
 const isSecondArraySquared = (firstArray, secondArray) => {
+    // Validating inputs to be arrays
     if (!Array.isArray(firstArray) || !Array.isArray(secondArray))
         throw new Error("arguments must be arrays");
 
+    // Check whether array lengths are equal
     if (firstArray.length !== secondArray.length) return false;
 
-    firstArray.sort();
-    secondArray.sort();
+    const lookUp = {};
 
-    for (const index in firstArray) {
-        let diff = firstArray[index] * firstArray[index] - secondArray[index];
-        if (diff !== 0) return false;
+    // Looping through the array - O(n)
+    for (const iterator of firstArray) {
+        const squaredValue = iterator * iterator;
+        // Mapping the squared value with it frequency
+        if (lookUp[squaredValue]) {
+            lookUp[squaredValue] += 1;
+        } else {
+            lookUp[squaredValue] = 1;
+        }
     }
-    return true;
+
+    for (const iterator of secondArray) {
+        // Look for the squared value
+        if (lookUp[iterator]) {
+            // Delete the property if frequency is one else reduce the frequency
+            if (lookUp[iterator] === 1) {
+                delete lookUp[iterator];
+            } else {
+                lookUp[iterator] -= 1;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    // Check whether lookup object is empty or not
+    return !Object.keys(lookUp).length;
 };
 
 export default isSecondArraySquared;
