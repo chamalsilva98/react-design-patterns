8ff2c11c16e25f7f979bf7b3fbd49ec78f40920b
Chamal Silva <chamalsilva98@gmail.com>
2022-04-19 15:06:32 +0530

    added comments with complexities, fixes #9

--- src/day-0-reverse-string.js
+++ src/day-0-reverse-string.js
@@ -1,8 +1,14 @@
 const reverseString = (str) => {
+    // Validating to be a string
+    if (typeof str !== "string") throw new Error("Input must be string");
+
     let reverse = "";
     const length = str.length;
+    // Looping through the string, time complexity - O(n)
     for (let index = 0; index < length; index++) {
+        // Getting elements from the end of the string iteratively
         const element = str[length - index - 1];
+        // Appending elements in reverse order, space complexity - O(n)
         reverse += element;
     }
     return reverse;
--- src/day-1-reverse-string-extended.js
+++ src/day-1-reverse-string-extended.js
@@ -1,10 +1,5 @@
+import reverseString from "./day-0-reverse-string";
+
 String.prototype.reverseString = function () {
-    const str = this;
-    let reverse = "";
-    const length = str.length;
-    for (let index = 0; index < length; index++) {
-        const element = str[length - index - 1];
-        reverse += element;
-    }
-    return reverse;
+    return reverseString(this);
 };
