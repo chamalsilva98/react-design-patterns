c6263d31cdbee2442a24d2b9f9e1863e2a9f7d95
Chamal Silva <chamalsilva98@gmail.com>
2022-07-05 00:28:26 +0530

    smooth transition on mouse enter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import "./App.css";
 
 function App() {
+  const [style, setStyle] = React.useState({});
+
+  const handleChange = () => {
+    const randomHeight = (Math.random() * 10000) % (window.innerHeight - 60);
+    const randomWidth = (Math.random() * 10000) % (window.innerWidth - 300);
+    setStyle({
+      transform: `translate(${randomWidth}px,${randomHeight}px)`,
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div className="days-react" style={style} onMouseEnter={handleChange}>
+        30 Days of React
+      </div>
+    </React.Fragment>
   );
 }
 
